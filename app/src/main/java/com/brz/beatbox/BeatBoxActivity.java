c72439a8ca00f45e1fe9803900a7c754d798f243
Bogdan Zhuravlov <arsbogdanus@gmail.com>
2024-06-28 13:41:21 +0300

    Data binding and MVVM (Part 2)

--- app/src/main/java/com/brz/beatbox/BeatBoxActivity.java
+++ app/src/main/java/com/brz/beatbox/BeatBoxActivity.java
@@ -2,7 +2,7 @@ package com.brz.beatbox;
 
 import androidx.fragment.app.Fragment;
 
-public class BeatBoxActivity  extends SingleFragmentActivity {
+public class BeatBoxActivity extends SingleFragmentActivity {
     @Override
     protected Fragment createFragment() {
         return BeatBoxFragment.newInstance();
--- app/src/main/java/com/brz/beatbox/BeatBoxFragment.java
+++ app/src/main/java/com/brz/beatbox/BeatBoxFragment.java
@@ -10,26 +10,37 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.brz.beatbox.databinding.FragmentBeatBoxBinding;
 import com.brz.beatbox.databinding.ListItemSoundBinding;
 
+import java.util.List;
+
 public class BeatBoxFragment extends Fragment {
+    private BeatBox mBeatBox;
+
     public static BeatBoxFragment newInstance() {
         return new BeatBoxFragment();
     }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mBeatBox = new BeatBox(getActivity());
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        com.brz.beatbox.databinding.FragmentBeatBoxBinding binding = DataBindingUtil.inflate(inflater, R.layout.fragment_beat_box, container, false);
-
+        FragmentBeatBoxBinding binding = DataBindingUtil.inflate(inflater, R.layout.fragment_beat_box, container, false);
         binding.recyclerView.setLayoutManager(new GridLayoutManager
             (getActivity(), 3));
-        binding.recyclerView.setAdapter(new SoundAdapter());
-
+        binding.recyclerView.setAdapter(new SoundAdapter(mBeatBox.getSounds()));
         return binding.getRoot();
     }
 
     private class SoundHolder extends RecyclerView.ViewHolder {
         private ListItemSoundBinding mBinding;
+
         private SoundHolder(ListItemSoundBinding binding) {
             super(binding.getRoot());
             mBinding = binding;
@@ -37,6 +48,12 @@ public class BeatBoxFragment extends Fragment {
     }
 
     private class SoundAdapter extends RecyclerView.Adapter<SoundHolder> {
+        private List<Sound> mSounds;
+
+        public SoundAdapter(List<Sound> sounds) {
+            mSounds = sounds;
+        }
+
         @Override
         public SoundHolder onCreateViewHolder(ViewGroup parent, int viewType) {
             LayoutInflater inflater = LayoutInflater.from(getActivity());
@@ -44,12 +61,14 @@ public class BeatBoxFragment extends Fragment {
                 .inflate(inflater, R.layout.list_item_sound, parent, false);
             return new SoundHolder(binding);
         }
+
         @Override
         public void onBindViewHolder(SoundHolder holder, int position) {
         }
+
         @Override
         public int getItemCount() {
-            return 0;
+            return mSounds.size();
         }
     }
 }
