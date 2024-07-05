589edd5c7b8e1c45171f78b3cf0676d7cb978269
Bogdan Zhuravlov <arsbogdanus@gmail.com>
2024-07-05 14:10:17 +0300

    Modular implementation and acoustic impact (Part 1)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,7 +36,7 @@ android {
 }
 
 dependencies {
-
+    testImplementation ("org.mockito:mockito-core:4.3.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
---
+++ app/src/test/java/com/brz/beatbox/SoundViewModelTest.java
@@ -0,0 +1,19 @@
+package com.brz.beatbox;
+
+import static org.mockito.Mockito.mock;
+
+import org.junit.Before;
+
+public class SoundViewModelTest {
+    private BeatBox mBeatBox;
+    private Sound mSound;
+    private SoundViewModel mSubject;
+
+    @Before
+    public void setUp() throws Exception {
+        mBeatBox = mock(BeatBox.class);
+        mSound = new Sound("assetPath");
+        mSubject = new SoundViewModel(mBeatBox);
+        mSubject.setSound(mSound);
+    }
+}
